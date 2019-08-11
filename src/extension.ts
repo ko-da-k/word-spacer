e47e60ba12a50e8cc42789b7d0bb26c1e2d8d3f3
kyo-bad <di.aps53@gmail.com>
2019-08-11 17:43:54 +0900

    refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -10,21 +10,6 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "word-spacer" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-
-	context.subscriptions.push(
-		vscode.commands.registerCommand(
-			'extension.helloWorld', () => {
-				// The code you place here will be executed every time your command is executed
-
-				// Display a message box to the user
-				vscode.window.showInformationMessage('Hello World!');
-			}
-		)
-	);
-
 	context.subscriptions.push(
 		vscode.commands.registerCommand(
 			'extension.insertSpace', () => {
@@ -39,23 +24,14 @@ const insertSpace = () => {
 	if (!editor) {
 		return
 	}
-	// 拡張子チェック
-	let fileTypes = editor.document.fileName.split(".")[-1];
-	const suffixArray = [".md"]
-	if (suffixArray.includes(fileTypes)) {
-		vscode.window.showInformationMessage(`You cannot use WordSpacer extension at ${fileTypes} filetypes`);
-		return
-	}
-	console.log("Not Implemented")
-	let hoge = editor.document.getText()
-	let lineCnt = editor.document.lineCount
-	console.log(hoge)
+	const lineCnt = editor.document.lineCount
 	editor.edit((editBuilder: vscode.TextEditorEdit) => {
-		for (let i = 0; i < lineCnt; i++) {
+		for (let lineIdx = 0; lineIdx < lineCnt; lineIdx++) {
 			let lineText = editor.document.lineAt(i)
-			for (let j = 0; j < lineText.text.length - 1; j++) {
-				if ((isJapanese(lineText.text[j]) && isEnglish(lineText.text[j + 1])) || (isEnglish(lineText.text[j]) && isJapanese(lineText.text[j + 1]))) {
-					editBuilder.insert(new vscode.Position(i, j + 1), " ");
+			for (let colIdx = 0; colIdx < lineText.text.length - 1; colIdx++) {
+				if (isJapaneseEnglishPair(lineText.text[colIdx], lineText.text[colIdx + 1])) {
+					// Insert space to the next character.
+					editBuilder.insert(new vscode.Position(lineIdx, colIdx + 1), " ");
 				}
 			}
 		}
@@ -71,24 +47,11 @@ const isEnglish = (str: string): boolean => {
 	return (str.match(/[A-Za-z0-9]/)) ? true : false
 };
 
-const getActiveLineText = (editor: vscode.TextEditor): string => editor.document.lineAt(editor.selection.active.line).text;
-
-const test = (editor: vscode.TextEditor) =>
-	edit(editor, (editBuilder: vscode.TextEditorEdit) => {
-		editBuilder.insert(new vscode.Position(editor.selection.active.line, 999), "hogehoge")
-	})
-
-const insertSpaceJapEng = (editor: vscode.TextEditor) =>
-	edit(editor, (editBuilder: vscode.TextEditorEdit) => {
-		editBuilder
-	})
-
-const edit = (editor: vscode.TextEditor, editFunc: (editBuilder: vscode.TextEditorEdit) => void) => {
-	editor.edit(editBuilder => {
-		editFunc(editBuilder);
-	});
+const isJapaneseEnglishPair = (first: string, second: string): boolean => {
+	const japEng = isJapanese(first) && isEnglish(second)
+	const engJap = isEnglish(first) && isJapanese(second)
+	return japEng || engJap
 }
-
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
